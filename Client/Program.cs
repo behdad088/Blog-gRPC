6f3264a73b6123bc8cc0d070c59b3801ea6013eb
behdad kardgar <behdad.kardgar1@gmail.com>
2023-04-13 12:35:08 +0200

    Create client and server project

---
+++ Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace Client
+{
+    internal class Program
+    {
+        const string target = "localhost:50052";
+        static async Task Main(string[] args)
+        {
+            var channel = new Channel(target, ChannelCredentials.Insecure);
+
+            await channel.ConnectAsync().ContinueWith((t) =>
+            {
+                if (t.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully.");
+                }
+            });
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
