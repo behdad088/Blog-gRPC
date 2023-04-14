ea7da861e814731ece7125242e427aff09b5eb85
behdad kardgar <behdad.kardgar1@gmail.com>
2023-04-14 07:14:57 +0200

    implement Blog server

--- Server/BlogServiceImpl.cs
+++ Server/BlogServiceImpl.cs
@@ -1,8 +1,32 @@
-﻿using static Blog.BlogService;
+﻿using Blog;
+using Grpc.Core;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using static Blog.BlogService;
 
 namespace server
 {
     internal class BlogServiceImpl : BlogServiceBase
     {
+        private static MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+        private static IMongoDatabase mongoDatabase = mongoClient.GetDatabase("mydb");
+        private static IMongoCollection<BsonDocument> mongoCollection = mongoDatabase.GetCollection<BsonDocument>("blog");
+
+        public override Task<CreaeteBlogResponse> CreateBlog(CreateBlogRequest request, ServerCallContext context)
+        {
+            var blog = request.Blog;
+            BsonDocument doc = new BsonDocument("author_id", blog.AuthorId)
+                .Add("title", blog.Title)
+                .Add("content", blog.Content);
+
+            mongoCollection.InsertOne(doc);
+            var id = doc.GetValue("_id").ToString();
+            blog.Id = id;
+
+            return Task.FromResult(new CreaeteBlogResponse
+            {
+                Blog = blog
+            });
+        }
     }
 }
